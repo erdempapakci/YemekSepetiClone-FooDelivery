eef6fe71423ada57ca32413a6a786e578968d491
erdempapakci <erdmppkc@gmail.com>
2022-08-21 19:03:06 +0300

    List Cells designed

---
+++ YemekSepetiClone/CustomViews/TableViewCells/DishListTableViewCells/DishListTableViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  DishListTableViewCell.swift
+//  YemekSepetiClone
+//
+//  Created by Erdem Papakçı on 21.08.2022.
+//
+
+import UIKit
+
+class DishListTableViewCell: UITableViewCell {
+    
+    static let identifier = "DishListTableViewCell"
+
+    @IBOutlet weak var dishListImageView: UIImageView!
+    
+    @IBOutlet weak var dishListTitleLabel: UILabel!
+    
+    @IBOutlet weak var dishListDescriptionLabel: UILabel!
+
+    func setup(dish: Dish) {
+
+        dishListImageView.kf.setImage(with: dish.image?.asUrl)
+        dishListTitleLabel.text = dish.name
+        dishListDescriptionLabel.text = dish.description
+        
+        
+    }
+
+}
